ea65755bf49ae60e2de1c90b65cd6d5d86c8f857
Ajay Singh Thakurathi <ajayst53@gmail.com>
2020-03-17 03:45:52 +0530

    shell flyout header and common header impl

--- Forms/Forms/Custom/CommonHeader.xaml.cs
+++ Forms/Forms/Custom/CommonHeader.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Forms.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,12 @@ using Xamarin.Forms.Xaml;
 namespace Forms.Custom
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ShellHeader : ContentPage
+    public partial class CommonHeader : Grid
     {
-        public ShellHeader()
+        public CommonHeader()
         {
             InitializeComponent();
+            BindingContext = new BaseViewModel();
         }
     }
 }
---
+++ Forms/Forms/ViewModels/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Forms.ViewModels
+{
+	public class BaseViewModel : INotifyPropertyChanged
+	{
+
+		public BaseViewModel()
+		{
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null)
+				handler(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
