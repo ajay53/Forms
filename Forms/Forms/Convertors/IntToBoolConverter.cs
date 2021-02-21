ec0add7a228aa5f07fc19b87b89c57f2c8217470
Ajay Singh Thakurathi <ajayst53@gmail.com>
2021-02-22 01:51:53 +0530

    added convertor

---
+++ Forms/Forms/Convertors/IntToBoolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Forms.Convertors
+{
+    public class IntToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (int)value != 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? 1 : 0;
+        }
+    }
+}
